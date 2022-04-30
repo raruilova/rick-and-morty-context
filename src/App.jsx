892137bbe537ffe602a0cb08576e4b715577a99f
robinson <robinjosellove@gmail.com>
2022-04-30 18:22:30 -0500

    project structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+      <button className="button is-primary">Hi!</button>
+    </div>
+  )
+}
+
+export default App
